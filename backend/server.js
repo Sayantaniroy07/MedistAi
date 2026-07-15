5f748dcd1870e30cbd62d00b536f8a395dc6b8c2
Sayantani <sayantaniroy167@gmail.com>
2026-07-16 01:23:53 +0530

    Prepare backend for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,18 @@ dotenv.config();
 
 connectDB();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://medist-ai.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/chat", chatRoutes);
@@ -25,7 +35,15 @@ app.use(
 app.get("/test", (req, res) => {
   res.send("Server OK");
 });
-app.listen(5000, () => {
-  console.log("Server Running on Port 5000");
-  console.log("Gemini Key:", process.env.GEMINI_API_KEY);
+
+app.get("/", (req, res) => {
+  res.send("MedistAI Backend is Running 🚀");
+});
+
+// app.listen(5000, () => {
+//   console.log("Server Running on Port 5000");
+//   console.log("Gemini Key:", process.env.GEMINI_API_KEY);
+// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
